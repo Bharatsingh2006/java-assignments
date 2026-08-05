a1697c0f222bb6287f5ff7ce1e38ce425bf407bc
Bharatsingh2006 <bharat102006@gmail.com>
2026-08-05 09:08:56 +0530

    first commit

---
+++ firstproject/src/tri.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class tri {
+    public void area(int b , int h){
+    System.out.println("area" + (0.5*b*h));}
+
+
+    public void perimeter(int A , int B , int C){
+    System.out.println("perimeter" + (A+B+C));
+
+    
+}}
+
